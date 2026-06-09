66564c5fbea5ff70d759514fb50367f8ba68fb74
hubert <hubert.zajac92@gmail.com>
2026-06-09 13:42:53 +0200

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,31 +6,59 @@ import Image from "next/image";
 import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+
+  const handleMobileMenuSwitch = () => {
+    setIsMobileOpen((prev) => !prev);
+  };
+
   return (
     <header className={styles.header}>
       <div className={`${styles.navBox} container`}>
         <Link href="/">
           <div className={styles.logoContainer}>
             <Image src={logoImg} alt="logo" height={65} width={90} />
-            <div>
+            <div className={styles.titleBox}>
               <h1>
-                <span className={styles.line}>Anna Niemczewska</span>
-                <span className={styles.line}>Biuro Rachunkowe</span>
+                <span className={`${styles.line} ${styles.big}`}>
+                  Anna Niemczewska
+                </span>
+                <span className={`${styles.line} ${styles.small}`}>
+                  Biuro Rachunkowe
+                </span>
               </h1>
             </div>
           </div>
         </Link>
-        <nav>
-          <ul>
+        <nav className={styles.menu}>
+          <ul className={styles.menuItems}>
             <li>
-              <Link href="/">Strona główna</Link>
-              <Link href="/">Oferta</Link>
-              <Link href="/">Kontakt</Link>
+              <Link href="/" className={styles.menuItem}>
+                Strona Główna
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className={styles.menuItem}>
+                Oferta
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className={styles.menuItem}>
+                Kontakt
+              </Link>
             </li>
           </ul>
         </nav>
+        <div className={styles.iconsBox} onClick={handleMobileMenuSwitch}>
+          {isMobileOpen ? (
+            <IoClose size={40} color="#fff" />
+          ) : (
+            <GiHamburgerMenu size={40} color="#fff" />
+          )}
+        </div>
       </div>
     </header>
   );
